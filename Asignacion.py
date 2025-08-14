9f667b239b34141f21fa0b9444d4f205ee345900
gus <gustavogonza01.g@gmail.com>
2025-08-14 18:14:17 -0300

    probe la asignacion en python

---
+++ Asignacion.py
@@ -0,0 +1,8 @@
+probando_La_Asignacion = "Estoy"
+probando_La_Asignacion += " "
+probando_La_Asignacion += "aprendiendo"
+probando_La_Asignacion += " "
+probando_La_Asignacion += "a"
+probando_La_Asignacion += " "
+probando_La_Asignacion += "programar con python"
+print (probando_La_Asignacion)
